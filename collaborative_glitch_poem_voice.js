e3478238eec2dc986a852e8f0ac858b06b714302
Sarah Buckius <210062217+sbuckius@users.noreply.github.com>
2025-08-27 13:49:40 -0700

    Update collaborative_glitch_poem_voice.js

--- collaborative_glitch_poem_voice.js
+++ collaborative_glitch_poem_voice.js
@@ -71,8 +71,10 @@ function draw() {
 
   textSize(10);
   textAlign(LEFT);
-  text("Type three phrases about glitches including 1) an example of when you encounter them 2) how they make you feel 3) a word to describe them.", width / 8, 120);
-
+  text("Type three phrases about glitches including:", width / 8, 120);
+text("1) an example of when you encounter them 2) how they make you feel 3) a word to describe them.", width / 8, 123);
+  text("T2) how they make you feel", width / 8, 126);
+  text("3) a word to describe them.", width / 8, 130);
   drawPoemBox();
 }
 
